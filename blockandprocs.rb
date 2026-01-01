0ed470b8beb87c0e4de7077bd00b4286ddb2185e
uciharis <harisnop@gmail.com>
2026-01-01 23:54:23 +0700

    contoh tambahan proc dijadikan parameter callback

--- blockandprocs.rb
+++ blockandprocs.rb
@@ -13,9 +13,28 @@
 # test.call(9)
 
 # contoh lain proc
-doYouLike = Proc.new do |aGoodThing|
-  puts 'i really like ' + aGoodThing
+# doYouLike = Proc.new do |aGoodThing|
+#   puts 'i really like ' + aGoodThing
+# end
+
+# doYouLike.call('chocolate')
+# doYouLike.call('ruby programming')
+
+# Contoh lain proc
+
+def do_self_Important (some_proc)
+  puts 'everybody just HOLD on, i have something to do'
+    some_proc.call
+    puts 'ok, im done. continue your work'
 end
 
-doYouLike.call('chocolate')
-doYouLike.call('ruby programming')
+say_hello = Proc.new do
+  puts 'hello there'
+end
+
+say_goodbye = Proc.new do
+  puts 'goodbye now'
+end
+
+do_self_Important(say_hello)
+do_self_Important(say_goodbye)
