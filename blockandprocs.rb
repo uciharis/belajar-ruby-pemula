839c57a756d5c6024b2291b33624db464b04ddbf
uciharis <harisnop@gmail.com>
2026-01-04 00:46:10 +0700

    makin mumet njir

--- blockandprocs.rb
+++ blockandprocs.rb
@@ -22,19 +22,93 @@
 
 # Contoh lain proc
 
-def do_self_Important (some_proc)
-  puts 'everybody just HOLD on, i have something to do'
-    some_proc.call
-    puts 'ok, im done. continue your work'
+# def do_self_Important (some_proc)
+#   puts 'everybody just HOLD on, i have something to do'
+#     some_proc.call
+#     puts 'ok, im done. continue your work'
+# end
+
+# say_hello = Proc.new do
+#   puts 'hello there'
+# end
+
+# say_goodbye = Proc.new do
+#   puts 'goodbye now'
+# end
+
+# do_self_Important(say_hello)
+# do_self_Important(say_goodbye)
+# 
+# def maybeDo someProc
+#   if rand(2) == 0
+#     someProc.call
+#   end
+# end
+
+# def twiceDo someProc
+#   someProc.call
+#   someProc.call
+# end
+
+# wink = Proc.new do
+#   puts '<twink>'
+# end
+
+# glance = Proc.new do
+#   puts '<glance>'
+# end
+
+# maybeDo wink
+# maybeDo glance
+# maybeDo glance
+# maybeDo wink
+# 
+# def doUntilFalse firstInput, someProc
+#   input = firstInput
+#   output = firstInput
+
+#   while output
+#     input = output
+#     output = someProc.call(input)
+#   end
+
+#   input
+# end
+
+# buildArrayOfSquares = Proc.new do |array|
+#   lastNumber = array.last
+#   if lastNumber <= 0
+#     false
+#   else
+#     array.pop
+#     array.push lastNumber * lastNumber
+#     array.push lastNumber-1
+#   end
+# end
+
+# always_false = Proc.new do |justIgnoreMe|
+#   false
+# end
+
+# puts doUntilFalse([5], buildArrayOfSquares).inspect
+# puts doUntilFalse("i am not false", always_false)
+
+def compose proc1, proc2
+  Proc.new do |x|
+    proc2.call(proc1.call(x))
+  end
 end
 
-say_hello = Proc.new do
-  puts 'hello there'
+squareIt = Proc.new do |x|
+  x * x
 end
 
-say_goodbye = Proc.new do
-  puts 'goodbye now'
+doubleIt = Proc.new do |x|
+  x + x
 end
 
-do_self_Important(say_hello)
-do_self_Important(say_goodbye)
+dobelthensquare = compose doubleIt, squareIt
+squarethendouble = compose squareIt, doubleIt
+
+puts dobelthensquare.call(5)
+puts squarethendouble.call(5)
--- intro.rb
+++ intro.rb
@@ -8,9 +8,24 @@
 
 # puts 'kopak'.center(9,'-')
 
-linewidth = 20
-str = '--jokontol--'
-puts str.ljust(linewidth,'-')
-puts str.ljust(10,'-')
-puts str.rjust(linewidth,'-')
-puts str.rjust(10,'-')
+# linewidth = 20
+# str = '--jokontol--'
+# puts str.ljust(linewidth,'-')
+# puts str.ljust(10,'-')
+# puts str.rjust(linewidth,'-')
+# puts str.rjust(10,'-')
+
+# nama_pengguna = 'nganu'
+# usia = 69
+# puts "nama sya #{nama_pengguna}, usia #{usia} tahun"
+# puts nama_pengguna + ' ' + usia.to_s
+# puts usia.class
+# puts nama_pengguna.class
+# hobi = [:coding, :ngopi]
+# puts hobi.class #Array
+
+5.times {print 'sempak'}
+menu = ['nasgor','nasdang', 'sate']
+menu.each { |makanan| print ' ' + makanan.capitalize + ' '}
+berat_badan = 1003,5
+puts berat_badan.class
