a2bb1f767a46f1d1a7bd78b198c4e1ee8c2d7e73
uciharis <harisnop@gmail.com>
2026-01-05 19:01:23 +0700

    tambah source belajar

--- rubynew.rb
+++ rubynew.rb
@@ -1,5 +1,5 @@
+# source : ruby-doc.org
 =begin
-
 
 p 'gin joint'.length
 p 'jokowi'.index('x')
@@ -17,6 +17,7 @@ puts "good morning,\ngrandpa"
 
 =end
 
+=begin
 # arrays + hashes
 buah = ['apel', 'mangga', 'jeruk'] # contoh array
 puts buah[0]
@@ -41,6 +42,7 @@ p ini_array_kosong
 # membuat array dengan kata/kalimat
 hewan = %w{ kucing anjing burung ular } # %w adalah cara cepat membuat array
 # %w adalah huruf kecil w
+# menggunakan curly braces {}
 p hewan
 
 role = "ngademin"
@@ -48,3 +50,37 @@ dataW = %W{halo dunia #{role} \n}
 p dataW
 dataw = %w{halo dunia #{role} \n}
 p dataw
+
+# hashes
+# 
+# format hashes diatas menggunakan string sebagai key
+biodata = {
+  'nama' => 'jokowi',
+  'jabatan' => 'presiden',
+  'tahun' => 2014,
+  'rekor' => 'raja korupsi',
+}
+p biodata['nama']
+p biodata['rekor']
+
+# membuat hashes dengan symbol sebagai key
+biodata_wowo = {
+  nama: 'praboow',
+  jabatan: 'presiden',
+  rekor: 'raja sawit',
+}
+# nama: sama dengan :nama => 'praboow'
+p biodata_wowo[:nama]
+p biodata_wowo[:rekor]
+
+=end
+
+# kontrol alur
+count = 9
+if count > 10
+  puts 'coba lagi'
+elsif count == 3
+    puts 'kamu gagal'
+else
+    puts 'masukkan nomor'
+end
