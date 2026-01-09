c96ea18992f6f1e641d59e8d334efd6864d07ed6
mikoc0k <uciharis@hotmail.com>
2026-01-10 01:03:35 +0700

    new chapter ruby

---
+++ ruby_new.rb
@@ -0,0 +1,82 @@
+# Some basic Ruby
+
+=begin
+def say_goodnight(name)
+  'goodnight, ' + name
+end
+
+puts say_goodnight('Alice')
+puts say_goodnight('Bob')
+
+=end
+
+=begin
+
+# cara membuat array
+# ada 2 cara
+# cara-1 :
+array1 = []
+array1[0] = 'kambing'
+array1[1] = 'kodok'
+print array1
+puts "\n -------"
+# cara-2 :
+array2 = Array.new
+array2[0] = 'jeruk'
+array2[1] = 'nanas'
+print array2
+
+=end
+
+=begin
+
+# cara membuat hash
+
+# cara-1 :
+# simbol sebagai key
+presiden_gemblung = {
+  nama: 'jokowee',
+  skill: 'goblok pasif',
+  harta: 1000000000,
+}
+
+print "\n #{presiden_gemblung}"
+
+# cara-2 :
+# hash rocket => sebagai key
+presiden_kita = {
+  'nama' => 'sby',
+  'skill' => 'pelukis aktif',
+  'harta' => 2000000000,
+}
+
+print "\n #{presiden_kita}"
+
+
+# cara -3 :
+# dengan Hash.new
+presiden_baru = Hash.new
+presiden_baru['nama'] = 'owo' # string sebagai key
+presiden_baru['skill'] = 'pecinta saweet'
+presiden_baru[:kolor] = 'merah' # simbol sebagai key
+print "\n #{presiden_baru}"
+
+
+# cara -4 :
+# menggunakan method Hash[] daftar argumen
+buah_favorit = Hash[:satu, 'apel', :dua, 'jeruk']
+print buah_favorit
+
+
+# cara -5 :
+# pakai shorthand
+nama = 'jock o wee'
+skill = 'nipu negoro'
+presiden_palsu = {
+  nama:,
+  skill:,
+  }
+  
+print presiden_palsu
+  
+=end
